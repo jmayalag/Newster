1b4f0a054cb7c67785a2087b96f6c53fd6a9f9ee
Jordan Ayala <jmayala@protonmail.com>
2020-12-22 20:06:50 -0300

    Add toolbar

--- app/src/main/java/com/hodinkee/hodinnews/MainActivity.kt
+++ app/src/main/java/com/hodinkee/hodinnews/MainActivity.kt
@@ -2,12 +2,23 @@ package com.hodinkee.hodinnews
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.hodinkee.hodinnews.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar
+            .setupWithNavController(navController, appBarConfiguration)
     }
 }
